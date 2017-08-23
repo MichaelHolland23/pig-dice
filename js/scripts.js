3d6a2cc996094a0268c3bd44e828a007fa6f1759
Mike Holland and Nate McGregor <nate@>
2017-08-23 09:34:06 -0700

    create html, basic layout for front and back end

---
+++ js/scripts.js
@@ -0,0 +1,46 @@
+//Back-end
+
+function player(total,round){
+  this.total = total;
+  this.round = round;
+};
+
+player.prototype.rollDice = function() {
+  var roll = Math.floor((Math.random()*6)+1);
+  if(roll===1){
+    alert("You rolled a 1! Your round score will be reset to zero, and it is the next player's turn");
+    this.round = 0;
+  }else{
+    this.round += roll;
+  }
+  return this.round;
+};
+
+player.prototype.addToTotal = function() {
+  this.total += this.round;
+};
+
+
+//Front end
+$(document).ready(function() {
+  var total = 0;
+  var round = 0;
+  $("#pigdice").submit(function(event) {
+  event.preventDefault();
+  //submit function
+  var player1 = new player(total,round);
+  var player2 = new player(total,round);
+
+  //game
+  alert(player1.rollDice());
+  }
+}
+
+//submit function
+var player1 = new player(total,round);
+var player2 = new player(total,round);
+
+//game
+alert(player1.rollDice());
+
+player1.addToTotal();
